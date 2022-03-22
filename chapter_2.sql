3a36c804fb4c7bd0531951a29534b862348040d5
Pete Es <piotrpiotreksocha@gmail.com>
2022-03-22 17:59:48 +0100

    chapter_2 final update

--- chapter_2.sql
+++ chapter_2.sql
@@ -7,3 +7,28 @@ SELECT
 	COUNT(*) AS number_of_invoices,
     SUM(invoice_total) AS grand_invoice_total
 FROM invoices;
+
+-- -------------------------------------------------- ex 1
+
+SELECT vendor_name, vendor_city, vendor_state
+FROM vendors
+ORDER BY vendor_name;
+
+-- -------------------------------------------------- ex 2
+
+SELECT COUNT(*) AS number_of_invoices,
+    SUM(invoice_total - payment_total - credit_total) AS total_due
+FROM invoices
+WHERE invoice_total - payment_total - credit_total > 0;
+
+-- -------------------------------------------------- ex 3
+
+SELECT vendor_name, vendor_city
+FROM vendors
+WHERE vendor_id = 34;
+
+SELECT COUNT(*) AS number_of_invoices,
+    SUM(invoice_total - payment_total - credit_total) AS total_due
+FROM invoices
+WHERE vendor_id = 34;
+
